7f5e86bba976628680d1a4641de8303b5fc8204d
Julia <juliauniver145@gmail.com>
2022-12-25 22:16:58 +0300

    Part1

---
+++ Final/FinalWork/Program.cs
@@ -0,0 +1,20 @@
+﻿string[] array1 = new string[4] { "hello", "2", "world", ":)" };
+string[] array2 = new string[array1.Length];
+int number = 0;
+
+
+for (int i = 0; i < array1.Length; i++)
+{
+    if (array1[i].Length <= 3)
+    {
+        array2[number] = array1[i];
+        number++;
+    }
+}
+
+for (int i = 0; i < array2.Length; i++)
+{
+    Console.Write($"{array2[i]} ");
+}
+
+
